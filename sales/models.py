67764590a920352624fe7f8d7c0b7abc19378182
Mario Winter <mariowinter.sg@gmail.com>
2024-10-06 22:23:15 +0200

    feat(sales): add str methods to Customer, Product, Bill, Order, and Producttype models for string representation

--- sales/models.py
+++ sales/models.py
@@ -8,16 +8,23 @@ class Customer(models.Model):
     email_address = models.CharField(max_length=30, blank=True, default="")
     account = models.FloatField(blank=True, null=True)
     # one-to-many Order
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+        
 
 class Product(models.Model):
     name = models.CharField(max_length=30)
     price = models.FloatField()
     #many-to-many Order
+    def __str__(self):
+        return f"{self.name} {self.price}"
 
 class Bill(models.Model):
     total_amount = models.FloatField()
     is_paid = models.BooleanField(default=True)
     #one-to-one Order
+    def __str__(self):
+        return f"{self.total_amount} {self.is_paid}"
 
 class Order(models.Model):
     #many-to-one Customer
@@ -25,9 +32,13 @@ class Order(models.Model):
     product = models.ManyToManyField(Product, through="Producttype")
     #one-to-one Bill
     bill = models.OneToOneField(Bill, on_delete=models.CASCADE)
+    def __str__(self):
+        return f"{self.customer} {self.product} {self.bill}"
 
 
 class Producttype(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     type_name = models.CharField(max_length=300)
+    def __str__(self):
+        return f"{self.order} {self.product} {self.type_name}"
