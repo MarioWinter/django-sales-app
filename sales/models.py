5c1702f8cbecbc25cfc95ee2a095e3a5857d008c
Mario Winter <mariowinter.sg@gmail.com>
2024-10-06 15:16:58 +0200

    feat(sales): add Product, Bill, Order, and Producttype models with relationships

--- sales/models.py
+++ sales/models.py
@@ -7,3 +7,27 @@ class Customer(models.Model):
     newsletter_abo = models.BooleanField(default=True)
     email_address = models.CharField(max_length=30, blank=True, default="")
     account = models.FloatField(blank=True, null=True)
+    # one-to-many Order
+
+class Product(models.Model):
+    name = models.CharField(max_length=30)
+    price = models.FloatField()
+    #many-to-many Order
+
+class Bill(models.Model):
+    total_amount = models.FloatField()
+    is_paid = models.BooleanField(default=True)
+    #one-to-one Order
+
+class Order(models.Model):
+    #many-to-one Customer
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    product = models.ManyToManyField(Product, through="Producttype")
+    #one-to-one Bill
+    bill = models.OneToOneField(Bill, on_delete=models.CASCADE)
+
+
+class Producttype(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    type_name = models.CharField(max_length=300)
