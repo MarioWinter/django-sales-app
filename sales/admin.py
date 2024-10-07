7bb70c141cab95dd9b06f9e5c2067d209dfbfc52
Mario Winter <mariowinter.sg@gmail.com>
2024-10-07 23:33:28 +0200

    feat(sales): add admin interface for Customer, Bill, Order, Product, and Producttype models with custom settings

--- sales/admin.py
+++ sales/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from sales.models import Bill, Customer, Order, Product, Producttype
 
 # Register your models here.
+class CustomerAdmin(admin.ModelAdmin):
+    list_filter=["first_name", "last_name", "newsletter_abo"]
+    list_display=["first_name", "last_name", "account", "newsletter_abo", "email_address"]
+    search_fields=["first_name", "last_name", "newsletter_abo"]
+
+class ProductAdmin(admin.ModelAdmin):
+    list_filter=["name", "price"]
+    list_display=["name", "price"]
+    search_fields=["name"]
+    
+    
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Bill)
+admin.site.register(Order)
+admin.site.register(Product)
+admin.site.register(Producttype)
