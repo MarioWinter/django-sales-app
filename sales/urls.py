56f39271f6b46e33e765d27b58079c9ed942526b
Mario Winter <mariowinter.sg@gmail.com>
2024-10-12 20:17:02 +0200

    feat(sales): add URL configuration for sales app with customer list and search views

--- sales/urls.py
+++ sales/urls.py
@@ -1,3 +1,7 @@
 from django.urls import path
-#from .views import 
-urlpatterns = []
+from .views import CustomerListView, CustomerSearchNameView
+
+urlpatterns = [
+    path('', CustomerListView.as_view()),
+    path('<str:name>/', CustomerSearchNameView.as_view())
+]
--- sales/views.py
+++ sales/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+
+from .models import Customer
 
 # Create your views here.
+class CustomerListView(ListView):
+    model = Customer
+    #queryset = Customer.objects.all()
+    #queryset = Customer.objects.filter(first_name__icontains="John")
+    template_name = "sales/list.html"
+    context_object_name = "customers"
+    paginate_by = 3
+    
+class CustomerSearchNameView(CustomerListView):
+    def get_queryset(self):
+        name = self.kwargs.get("name")
+        return Customer.objects.filter(first_name__icontains=name)
