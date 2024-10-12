4fa46e9615a6d1a0d5692f200d371c298278499e
Mario Winter <mariowinter.sg@gmail.com>
2024-10-12 22:58:33 +0200

    feat(sales): add detail view for customers with last accessed timestamp update

--- sales/urls.py
+++ sales/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CustomerListView, CustomerSearchNameView
+from .views import CustomerListView, CustomerSearchNameView, CustomerDetailView
 
 urlpatterns = [
     path('', CustomerListView.as_view()),
-    path('<str:name>/', CustomerSearchNameView.as_view())
+    path('<str:name>/', CustomerSearchNameView.as_view()),
+    path('customer/<int:pk>', CustomerDetailView.as_view())
 ]
--- sales/views.py
+++ sales/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render, redirect
+from django.utils import timezone
+
+from django.db.models.base import Model as Model
+from django.shortcuts import redirect
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 from .models import Customer
 
@@ -7,6 +11,16 @@ from .models import Customer
 
 def redirect_to_sales_view(request):
     return redirect('sales/', permanent=True)
+
+class CustomerDetailView(DetailView):
+    model = Customer
+    template_name = "sales/detail.html"
+    context_object_name = "customer"
+    
+    def get_object(self):
+        obj = super().get_object()
+        obj.last_accessed = timezone.now()
+        return obj
 
 class CustomerListView(ListView):
     model = Customer
